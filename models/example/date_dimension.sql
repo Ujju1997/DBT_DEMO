8aaa83c39858f342a2b73946c4e95c053a59b5b7
ujjwal Tripathi <ujjwal.btripathi@gmail.com>
2026-05-28 11:17:39 +0000

    daily weather model created

--- models/example/date_dimension.sql
+++ models/example/date_dimension.sql
@@ -1,25 +1,13 @@
-with
-    cte as (
-        select
-            to_timestamp(started_at) as started_at,
-            date(to_timestamp(started_at)) as date_started_at,
-            hour(to_timestamp(started_at)) as hour_started_at,
-            case
-                when dayname(to_timestamp(started_at)) in ('Sat', 'Sun')
-                then 'WEEKEND'
-                else 'BUSINESSDAY'
-            end as day_type,
-            case
-                when month(to_timestamp(started_at)) in (12, 1, 2)
-                then 'WINTER'
-                when month(to_timestamp(started_at)) in (3, 4, 5)
-                then 'SPRING'
-                when month(to_timestamp(started_at)) in (6, 7, 8)
-                then 'SUMMER'
-                else 'AUTUMN'
-            end as station_of_year
-        from {{ source("DEMO", "BIKE") }}
-    )
+with cte as (
+select
+to_timestamp(started_at) as started_at,
+date(to_timestamp(started_at)) as date_started_at,
+hour(to_timestamp(started_at)) as hour_started_at,
+{{get_season('started_at')}} as season_of_the_year,
+{{day_type('started_at')}} as day_type
+
+from {{ source("DEMO", "BIKE") }}
+)
 
 select *
 from cte
